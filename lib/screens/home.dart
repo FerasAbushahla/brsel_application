7aaa53e7c8965243f92b74a2f39e8586b4e40f95
FerasAbushahla <abushahla21@gmail.com>
2022-08-10 15:31:15 +0300

    HomePage

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:brsel_application/componantes/myCustomAppBar.dart';
+import 'package:brsel_application/constants.dart';
+import 'package:brsel_application/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -13,14 +15,191 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             MyHomeCustomAppBar(),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              color: Colors.white,
+              width: SizeConfig.screenWidth,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(20, 9, 20, 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'العروض(130)',
+                          style: MyCustomTextStyle.myH2,
+                        ),
+                        Text(
+                          'تصفح الكل',
+                          style: MyCustomTextStyle.myGreenTextStyle,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      width: SizeConfig.screenWidth,
+                      height: 170,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Image.asset(
+                        'assets/images/Ad.jpg',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'المطاعم المتوفرة(120)',
+                    style: MyCustomTextStyle.myH2,
+                  ),
+                  Text(
+                    'تصفح الكل',
+                    style: MyCustomTextStyle.myGreenTextStyle,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SingleChildScrollView(
+              physics: const BouncingScrollPhysics(
+                  parent: AlwaysScrollableScrollPhysics()),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(
+                  AvailableRestList.length,
+                  (index) => Padding(
+                    padding: index == 0
+                        ? EdgeInsets.only(right: 20)
+                        : EdgeInsets.only(right: 10),
+                    child: availableRestautantsCard(),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'وجبات رائجة',
+                    style: MyCustomTextStyle.myH2,
+                  ),
+                  Text(
+                    'مشاهدة الكل',
+                    style: MyCustomTextStyle.myGreenTextStyle,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SingleChildScrollView(
+              physics: const BouncingScrollPhysics(
+                  parent: AlwaysScrollableScrollPhysics()),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(
+                  AvailableRestList.length,
+                  (index) => Padding(
+                    padding: index == 0
+                        ? EdgeInsets.only(right: 20)
+                        : EdgeInsets.only(right: 10),
+                    child: availableRestautantsCard(),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Container availableRestautantsCard() {
+    return Container(
+      width: 76,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: SizeConfig.screenWidth,
+            height: 76,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Image.asset(
+              'assets/images/Restaurant img.jpg',
+              fit: BoxFit.contain,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            'كنتاكي',
+            style: MyCustomTextStyle.myCardTitleBlackSecondTextStyle,
+          )
+        ],
+      ),
+    );
+  }
+
+  Container featuredMealsCard() {
+    return Container(
+      width: 76,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: SizeConfig.screenWidth,
+            height: 76,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Image.asset(
+              'assets/images/Restaurant img.jpg',
+              fit: BoxFit.contain,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            'كنتاكي',
+            style: MyCustomTextStyle.myCardTitleBlackSecondTextStyle,
+          )
+        ],
       ),
     );
   }
